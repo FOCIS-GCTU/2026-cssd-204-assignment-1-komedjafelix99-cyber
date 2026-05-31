6cac3281ee55a4d626b8eb7810ae789ea0ec2ae0
KOMEDJA <komedjafelix99@gmail.com>
2026-05-31 01:19:32 -0700

    Finished assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,40 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Felix Mayor Komedja
+# STUDENT ID:  2425402989
+# Email: komedjafelix99@gmail.com
+# Grader: Augustus
+#
+# On my honor, Felix Mayor Komedja, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # defining the main function
 
+    print()
+
+    print("...FKM")
+
+    print()
+
+    print("...FFFFFFFFFFFF........KKKK....KKKK........MM........MM.....")
+    print("...FFFFFFFFFFFF........KKKK...KKK..........MMM......MMM.....")
+    print("...FFFFFFFFFFFF........KKKK..KKK...........MMMM....MMMM.....")
+    print("...FFFF................KKKK.KKK............MM.MM..MM.MM.....")
+    print("...FFFF................KKKKKK..............MM...M.M..MM.....")
+    print("...FFFFFFFFFFFF........KKKKKK..............MM...MM...MM.....")
+    print("...FFFFFFFFFFFF........KKKK.KKK............MM........MM.....")
+    print("...FFFF................KKKK..KKK...........MM........MM.....")
+    print("...FFFF...........**...KKKK...KKK.....**...MM........MM...**")
+    print("...FFFF...........**...KKKK....KKKK...**...MM........MM...**")
+    print()
 
 
 main()
+
+
+
+
